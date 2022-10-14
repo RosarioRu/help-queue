ff882f2c07fc0fc0d15a6312826021a9c9f8589d
RosarioRu <mariarruval@gmail.com>
2022-10-14 16:21:26 -0700

    Update import statement for App since it was moved to components directory.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './components/App';
 
 //this code creates the root DOM node in which React will render our App component.
 
