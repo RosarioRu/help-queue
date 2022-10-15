53f4eb58adfb3cc5470a91df24f9da42a0c7068a
RosarioRu <mariarruval@gmail.com>
2022-10-15 15:06:20 -0700

    Add tickete and ticket list components.

---
+++ src/components/Ticket.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Ticket() {
+  const name = "Thato";
+  const name2 = "Haley";
+  return (
+    <react.Fragment>
+      <h3>2a</h3>
+      <h2>{name} and {name2}</h2>
+      <p><em>Firebase entries not saving!</em></p>
+      <hr/>
+    </react.Fragment>
+  );
+}
+
+export default Ticket;
